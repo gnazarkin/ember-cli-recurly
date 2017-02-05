62f04c66b9e692f14f3d25b0b0f76032e985fb0b
Eric Valadas <ericvaladas@gmail.com>
2017-02-05 16:42:59 -0500

    Use latest version of recurly.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   contentFor: function(type) {
     // we use body since Recurly must be available before
     if (type === 'body') {
-      return '<script type="text/javascript" src="https://js.recurly.com/v3/recurly.js"></script>';
+      return '<script type="text/javascript" src="https://js.recurly.com/v4/recurly.js"></script>';
     }
   }
 };
