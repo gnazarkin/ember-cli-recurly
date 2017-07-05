0c49fd4d92fe6859f0503f9eef5b51479c1503bd
Eric Valadas <ericvaladas@gmail.com>
2017-07-05 16:17:18 -0400

    Only configure recurly if it is defined

--- app/initializers/recurly-service.js
+++ app/initializers/recurly-service.js
@@ -5,7 +5,10 @@ export function initialize(container, application) {
   if(!config.recurly.publicKey) {
     throw new Ember.Error('RecurlyService: Missing Recurly key, please set `ENV.recurly.publicKey` in config.environment.js');
   }
-  recurly.configure(config.recurly.publicKey);
+
+  if (window.recurly) {
+    recurly.configure(config.recurly.publicKey);
+  }
 }
 
 export default {
